f8532696c1d77baa1077f8427316f0d99499b3be
Gurusharan Singh <gurusharansingh2222@gmail.com>
2025-04-27 18:10:49 +0530

    redis-v2

--- redis.js
+++ redis.js
@@ -1,10 +1,22 @@
+// lib/redis.js
+import 'dotenv/config'; // Load your .env first
 import Redis from 'ioredis';
 
+// Create Redis client
 const redis = new Redis({
   host: 'redis-16654.c305.ap-south-1-1.ec2.redns.redis-cloud.com',
   port: 16654,
-  password: process.env.REDIS_PASS, // Replace with your real password
-
+  password: process.env.REDIS_PASS,
+  
 });
 
-export default redis
+// Optional: Logs
+redis.on('connect', () => {
+  console.log('✅ Redis connected successfully');
+});
+
+redis.on('error', (err) => {
+  console.error('❌ Redis connection error:', err);
+});
+
+export default redis;
