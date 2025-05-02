6aeb192cbdfd4a8a1e6a489e702be479ba0a5281
Gurusharan Singh <gurusharansingh2222@gmail.com>
2025-05-02 18:49:34 +0530

    add cron job

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import 'dotenv/config'
 import connect from './config/Dbcoonect.js';
 import Userrouter from './routes/user.js';
 import Adminrouter from './routes/admin.js';
+import cron from 'node-cron';
+import fetch from 'node-fetch';
 
 import Bookrouter from './routes/book.js';
 import FavriouteRouter from './routes/favrioute.js';
@@ -24,16 +26,29 @@ connect()
 ConnectCloudinary();
 const Port=process.env.PORT || 8080
 
-// app.get('/',async(req,res)=>{
-//   await Notebooks.insertMany(data);
-//   res.send("Inserted");
 
-// })
+app.get('/', (req, res) => {
+  res.send('Server is running...');
+});
 // app.get('/delete',async(req,res)=>{
 //   await Orders.deleteMany();
 //   res.send("Deleted");
 
 // })
+
+
+
+// Schedule a task to run every 5 minutes
+cron.schedule('*/5 * * * *', async () => {
+  try {
+    const res = await fetch('https://cfbackend-ifij.onrender.com');
+    console.log('Pinged self to prevent sleeping:', res.status);
+  } catch (err) {
+    console.error('Error pinging server:', err);
+  }
+});
+
+
 
 app.use('/api',Userrouter);
 app.use('/api',Adminrouter);
